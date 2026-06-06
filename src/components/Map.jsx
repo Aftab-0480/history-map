4383e5a485de83471676236ee78a9501e9c0f801
Aftab-0480 <zeeshanamir927@gmail.com>
2026-06-06 23:57:23 +0530

    Made map more functionable

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,8 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import historicalData from "../data/historicalData";
+import { useEffect } from "react";
+
+
 
 function Map({ year }) {
     const visibleLocations = historicalData.filter(
@@ -8,22 +11,54 @@ function Map({ year }) {
             year <= data.endYear
     );
 
+    function FlyToLocation({ location }) {
+        const map = useMap();
+
+        useEffect(() => {
+            if (location) {
+                map.flyTo(
+                    [location.lat, location.lng],
+                    8,
+                    {
+                        duration: 2
+                    }
+                );
+            }
+        }, [location, map]);
+
+        return null;
+    }
+
+    const selectedLocation =
+        visibleLocations.length === 1
+            ? visibleLocations[0]
+            : null;
+
     return (
         <div className="h-5/6 border bg-gray-800">
             <MapContainer
                 center={[22.5937, 78.9629]}
-                zoom={2}
+                zoom={4}
                 className="h-full w-full"
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
+                <FlyToLocation location={selectedLocation} />
+
                 {visibleLocations.map((location) => (
                     <Marker
                         key={location.title}
                         position={[location.lat, location.lng]}
-                    />
+                    >
+                        <Popup>
+                            <div>
+                                <h3 className="text-lg font-bold">{location.title}</h3>
+                                <p className="text-2xl text-gray-300">{location.location}</p>
+                            </div>
+                        </Popup>
+                    </Marker>
                 ))}
             </MapContainer>
         </div>
